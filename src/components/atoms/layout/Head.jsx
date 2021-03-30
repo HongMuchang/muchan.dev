f6e29268b50dd418c10f803435fb5a8ac18f1ca3
HongMuchan <muchang0824@gmail.com>
2021-03-30 15:17:14 +0900

    create #1 Head conponent

---
+++ src/components/atoms/layout/Head.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Head(props) {
+  const { title } = props
+
+  return (
+    <Head>
+      <title>{title}</title>
+      {/* <meta/> */}
+    </Head>
+  )
+}
+
+export default Head
