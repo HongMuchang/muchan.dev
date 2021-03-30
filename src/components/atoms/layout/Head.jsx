0ed806414a51a2e004f3779bbac493428273a048
HongMuchan <muchang0824@gmail.com>
2021-03-31 01:25:04 +0900

    Update #3 Head Component

--- src/components/atoms/layout/Head.jsx
+++ src/components/atoms/layout/Head.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-
-function Head(props) {
+export function Head(props) {
   const { title } = props
 
   return (
     <Head>
       <title>{title}</title>
-      {/* <meta/> */}
     </Head>
   )
 }
-
-export default Head
