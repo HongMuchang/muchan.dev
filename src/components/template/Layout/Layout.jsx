6ccf747e96e07a55fd7259462429764fad9afa2d
HongMuchan <muchang0824@gmail.com>
2021-04-01 00:14:42 +0900

    update #11  Layout Component

--- src/components/template/Layout/Layout.jsx
+++ src/components/template/Layout/Layout.jsx
@@ -1,4 +1,5 @@
-import { Head,Footer,Header } from '../../../components/atoms/layout/index'
+import { Head, Footer, Header } from '../../../components/atoms/layout/index'
+import styles from '../../../../styles/components/template/layout/layout.module.scss'
 
 export const Layout = (props) => {
   const { children, title } = props
@@ -6,7 +7,9 @@ export const Layout = (props) => {
     <div>
       {/* <Head title={title} /> */}
       <Header />
-        {children}
+      <main className={styles.main}>
+        <div className={styles.main_content}>{children}</div>
+      </main>
       <Footer />
     </div>
   )
