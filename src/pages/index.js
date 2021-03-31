ea825fe71812fa44c17d57ffe7560ddf9df0a33e
HongMuchan <muchang0824@gmail.com>
2021-04-01 00:11:41 +0900

    update #11  TopPage Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import { Layout } from '../components/template/Layout/Layout'
+import { BlogCard, Profile } from '../components/organisms/index'
 
+import styles from '../../styles/pages/index.module.scss'
 export default function Home() {
+  const bords = [1, 2]
+
   return (
     <>
       <Layout>
-        <p>hello</p>
+        <div className={styles.main}>
+          <Profile />
+          <ul>
+            {bords.map((bord, index) => (
+              <li key={index} className={styles.card}>
+                <a href="http://">
+                  <BlogCard title={'Hello'} day={'2020-12-12'} tag={'React'} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </Layout>
     </>
   )
