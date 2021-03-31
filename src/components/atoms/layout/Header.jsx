91e34df40bd66a8d653509eb4681b46c1988fdeb
HongMuchan <muchang0824@gmail.com>
2021-03-31 12:03:06 +0900

    update #5 Header Component

--- src/components/atoms/layout/Header.jsx
+++ src/components/atoms/layout/Header.jsx
@@ -1,23 +1,25 @@
 import Link from 'next/link'
-import styled from 'styled-components'
-import styles from './Header.module.scss'
+import styles from '../../../../styles/components/atoms/layout/Header.module.scss'
 
 export function Header() {
   return (
-    <header>
-      <p>muchan.dev</p>
-      <nav>
-        <Link href="/blog-page">
-          <a>Me</a>
-        </Link>
+    <header className={styles.header}>
+      <Link href="/s">
+        <a>
+          <p className={styles.header_title}>muchan.dev</p>
+        </a>
+      </Link>
+      <nav className={styles.header_nav}>
         <Link href="/blog-page">
           <a>About</a>
         </Link>
         <Link href="/blog-page">
           <a>Blog</a>
         </Link>
+        <Link href="/blog-page">
+          <a>Worsk</a>
+        </Link>
       </nav>
-      <p className={styles.p}>s</p>
     </header>
   )
 }
