3e4d64ad7fff4f0213f3d3951e5493b9d9da6b53
HongMuchan <muchang0824@gmail.com>
2021-04-01 00:15:33 +0900

    create #11  iconList Component

---
+++ src/components/ molecules/IconList.jsx
@@ -0,0 +1,36 @@
+import styles from '../../../styles/components/molecules/IconList.module.scss'
+import {
+  TwitterIcon,
+  QiitaIcon,
+  GithubIcon,
+  WontedlyIcon,
+} from '../atoms/icon/index'
+
+export function IconList() {
+  return (
+    <div>
+      <ul className={styles.icon_list}>
+        <li>
+          <a href="https://twitter.com/HongMuchan" target="blank">
+            <TwitterIcon />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/HongMuchang" target="blank">
+            <GithubIcon />
+          </a>
+        </li>
+        <li className={styles.qiita}>
+          <a href="https://qiita.com/HongMuchan" target="blank">
+            <QiitaIcon />
+          </a>
+        </li>
+        <li className={styles.wontedly}>
+          <a href="https://www.wantedly.com/id/muchan" target="blank">
+            <WontedlyIcon />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
