2b8be3663fd991fffea906dada28bd4541482399
HongMuchan <muchang0824@gmail.com>
2021-04-01 19:06:11 +0900

    update #15 icons alt rename

--- src/components/atoms/icon/QiitaIcon.jsx
+++ src/components/atoms/icon/QiitaIcon.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function QiitaIcon() {
   return (
     <div>
-      <Image src="/qiita.png" alt="muchan icon image" width={35} height={35} />
+      <Image src="/qiita.png" alt="Qiita icon" width={35} height={35} />
     </div>
   )
 }
--- src/components/atoms/icon/TSIcon.jsx
+++ src/components/atoms/icon/TSIcon.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function TSIcon() {
   return (
     <div>
-      <Image src="/ts.svg" alt="tsのアイコン画像" width={70} height={70} />
+      <Image src="/ts.svg" alt="TypeScriptのアイコン画像" width={70} height={70} />
     </div>
   )
 }
