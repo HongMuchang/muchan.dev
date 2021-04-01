e18036bddc640449b2fff13bf1fb7cad35a40fd8
HongMuchan <muchang0824@gmail.com>
2021-04-01 18:50:15 +0900

    create #13 About Page Component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,16 @@
 import { Layout } from '../components/template/Layout/Layout'
-import styles from '../../styles/pages/index.module.scss'
+import styles from '../../styles/pages/about.module.scss'
+import { AboutCard } from '../components/organisms/index'
 
 export default function About() {
   return (
     <>
       <Layout>
         <div className={styles.main}>
-          <p>About</p>
+          <div className={styles.main_content}>
+            {/* <Title title={'自己紹介'} /> */}
+            <AboutCard title={'こんいちは'} li={'ss'} dt={'sds'} dd={'dd'} />
+          </div>
         </div>
       </Layout>
     </>
