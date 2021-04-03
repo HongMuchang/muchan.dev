92f525a950048aec8ab45a90a1e506dfb3fcce05
HongMuchan <muchang0824@gmail.com>
2021-04-04 00:07:23 +0900

    update #21 Works Conponents

--- src/components/organisms/WorksCard.jsx
+++ src/components/organisms/WorksCard.jsx
@@ -2,27 +2,27 @@ import { Title } from '../atoms/works/index'
 import { WorksList, UrlList, GitList } from '../molecules/works/index'
 import styles from '../../../styles/components/organisms/WorksCard.module.scss'
 
-export function WorksCard() {
+export function WorksCard(props) {
+  const { title, git, gitUrl, link, linkUrl, addContent, langContent } = props
   return (
     <div className={styles.works}>
       <div className={styles.works_content}>
         <div className={styles.works_card}>
-          <Title title={'初めてのチーム制作'} />
+          <Title title={title} />
 
-          <div className={styles.link}>
-            <UrlList width={25} height={25} url={'https://hongmuchan.net'} />
-          </div>
+          {link && (
+            <div className={styles.link}>
+              <UrlList width={25} height={25} url={linkUrl} />
+            </div>
+          )}
 
-          <div className={styles.link}>
-            <GitList width={25} height={25} url={'https://hongmuchan.net'} />
-          </div>
-
-          <WorksList
-            title={'なぜ作ったのか？'}
-            content={'ssssssssssssssssssssss'}
-          />
-
-          <WorksList title={'使用言語'} content={'TypeScript、JS、FireBase'} />
+          {git && (
+            <div className={styles.link}>
+              <GitList width={25} height={25} url={gitUrl} />
+            </div>
+          )}
+          <WorksList title={'なぜ作ったのか？'} content={addContent} />
+          <WorksList title={'使用言語'} content={langContent} />
         </div>
       </div>
     </div>
