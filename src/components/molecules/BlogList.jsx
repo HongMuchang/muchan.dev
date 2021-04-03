ad21a0e522ab0d74fec8c269716c64fa7bc4012b
HongMuchan <muchang0824@gmail.com>
2021-04-03 20:46:24 +0900

    update #19 Blog Conponents

--- src/components/molecules/BlogList.jsx
+++ src/components/molecules/BlogList.jsx
@@ -1,12 +1,23 @@
+import styles from '../../../styles/components/molecules/blogs/BlogList.module.scss'
 import { Title, Tag, Day } from '../atoms/blogs/index'
 
 export function BlogList(props) {
   const { title, tag, day } = props
   return (
-    <div>
-      <Title title={title} />
-      <Tag tag={tag} />
-      <Day day={day} />
+    <div className={styles.content}>
+      <div>
+        <div className={styles.title}>
+          <Title title={title} />
+        </div>
+
+        <div className={styles.tag}>
+          <Tag tag={tag} />
+        </div>
+
+        <div className={styles.day}>
+          <Day day={day} />
+        </div>
+      </div>
     </div>
   )
 }
