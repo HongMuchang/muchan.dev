dde91e681ddf1912ae0c451a46fad91d4b9f9469
HongMuchan <muchang0824@gmail.com>
2021-04-04 00:06:54 +0900

    update #21 About Page

---
+++ src/components/date/about.js
@@ -0,0 +1,68 @@
+
+export const Basics = [
+  {title: "名前",content:"洪武昌 (ホン ムチャン)"},
+  {title: "呼び名",content:"むちゃん"},
+  {title: "学校",content:"HAL大阪"},
+  {title: "生年月日",content:"2000年8月24日"},
+  {title: "趣味",content:"フットサル、映画🍿、モンハン"},
+  {title: "特技",content:"韓国語が話せる事 🇰🇷"}
+]
+
+export const Informations = [
+  {content:"京都に住んでる専門学生、むちゃんです。"},
+  {content:"あだ名っぽいんですが、本名ですww。"},
+  {content:"普段はフロント好きで、フロントを書いてることが多いいです❗️❗️"},
+  {content:"個人ではUIUX（デザイン）にも興味があって勉強しています。"},
+  {content:"主に関西のイベントに参加したり登壇したりしています。" },
+  {content:"是非、会った時は声かけてください"},
+]
+
+export const Internships = [
+  {
+    office: "株式会社 TechBowl",
+    day: "2020/11 ~ 2021/2",
+    content: "Laravelで既存サービスの改善",
+    lang: "'Laravel、Figma",
+  },
+  {
+    office: "株式会社 タンバリン",
+    day: "2020/8 1Day",
+    content: "モブプロでボウリングシステムの開発",
+    lang: "JS",
+  },
+  {
+    office: "株式会社 サイバーエージェント",
+    day: "2021/3 2Days",
+    content: "ECサイトの作成　(CA Tech Challenge)",
+    lang: "React",
+  },
+  {
+    office: "株式会社 スリーシェイク",
+    day: "2021/3 インターン中",
+    content: "新規プロダクトの開発 (フロントエンド)",
+    lang: "NextJs、TypeScript、Firebase",
+  },
+]
+
+export const Activitys = [
+  {title: "アルバイト",content:"CA Tech Kids(メンター) ※2Year"},
+  {title: "アルバイト",content:"DMM WEB CAMP(メンター) ※2Months"},
+  {title: "学生団体　",content:"Volare ※2Year"},  
+]
+
+export const Todos = [
+  {content:"パフォーマンスチューニング"},
+  {content:"アクセシビリティ"},
+  {content:"Recoil"},
+  {content:"Go"},  
+  {content:"Webpack"},  
+  {content:"Babel"},  
+  {content:"OSS開発に参加"},  
+  {content:"UIUXの勉強"},
+  {content:"インフラのお勉強"},
+  {content:"TypeScript"},
+  {content:"C"},
+  {content:"Docker"},
+  {content:"CI"},
+  {content:"ios開発"},
+]
