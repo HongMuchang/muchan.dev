0e9e6b0a8cd8a588361d2023a3bd689ff334e940
HongMuchan <muchang0824@gmail.com>
2021-04-04 11:47:15 +0900

    update lib conponents

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,32 @@
 //index.js
+
+//全権取得
+export async function getAllPostsData() {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  }
+  const data = await fetch('https://muchan.microcms.io/api/v1/blog', key)
+  const posts = await data.json()
+  return posts
+}
+
+export async function getAllPostIds() {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  }
+  const data = await fetch('https://muchan.microcms.io/api/v1/blog', key)
+    .then((res) => res.json())
+    .catch(() => null)
+  const paths = data.contents.map((content) => `/blog/${content.id}`)
+  return paths
+}
+
+export async function getPostData(context) {
+  const id = context.params.id
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  }
+  const res = await fetch('https://muchan.microcms.io/api/v1/blog/' + id, key)
+  const data = res.json()
+  return data
+}
