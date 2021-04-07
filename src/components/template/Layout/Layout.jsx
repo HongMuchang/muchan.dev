e53cc8cb6fcc719012cfa1af1452acd92e6de113
HongMuchan <muchang0824@gmail.com>
2021-04-07 12:43:56 +0900

    update #25 Layout Components

--- src/components/template/Layout/Layout.jsx
+++ src/components/template/Layout/Layout.jsx
@@ -1,15 +1,21 @@
-import { Head, Footer, Header } from '../../../components/atoms/layout/index'
+import Head from 'next/head'
+import { Footer, Header, Meta } from '../../../components/atoms/layout/index'
+
 import styles from '../../../../styles/components/template/layout/layout.module.scss'
 
 export const Layout = (props) => {
-  const { children, title } = props
+  const { children, title = 'むちゃん' } = props
   return (
-    <div>
+    <html lang="ja">
+      <Head>
+        <title>{title}</title>
+        <Meta />
+      </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.main_content}>{children}</div>
       </main>
       <Footer />
-    </div>
+    </html>
   )
 }
