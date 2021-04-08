1290c8d5ad5844a56eb1d774b6a6f718ea37af6e
HongMuchan <muchang0824@gmail.com>
2021-04-08 21:54:24 +0900

    [update] scrol24

--- src/components/template/Layout/Layout.jsx
+++ src/components/template/Layout/Layout.jsx
@@ -9,6 +9,7 @@ export const Layout = (props) => {
     <html lang="ja">
       <Head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0,user-scalable=no" />
         <Meta />
       </Head>
       <Header />
